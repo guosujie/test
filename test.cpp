68cbd928d29288e728d287d97b535c121feb6ce8
guosujie <43333719+guosujie@users.noreply.github.com>
2020-05-11 21:42:02 +0800

    Update and rename test to test.cpp

--- test.cpp
+++ test.cpp
@@ -30,7 +30,5 @@ int main ()
 	MPI_Finalize ();
 	return 0;
 }
-————————————————
-版权声明：本文为CSDN博主「度凡心」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/qq_30239975/article/details/77703321
+
                 
